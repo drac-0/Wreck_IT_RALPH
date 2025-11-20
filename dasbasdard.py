7da7fca7ed3868f9d0d61be49cd46ab15c8713a2
drac-0 <muhammadfaiz050607@gmail.com>
2025-11-20 07:29:06 +0700

    IDK anymore

--- dasbasdard.py
+++ dasbasdard.py
@@ -19,15 +19,19 @@ def connect(host, port):
     return s
 
 def tunnel(s, host, port):
-    req = f"CONNECT {host}:{port} HTTP/1.1\r\nHost: {host}\r\n\r\n"
-    s.sendall(req.encode())
+	try :
+		req = f"CONNECT {host}:{port} HTTP/1.1\r\nHost: {host}\r\n\r\n"
+		s.sendall(req.encode())
 
-    data = b""
-    while b"\r\n\r\n" not in data:
-        data += s.recv(4096)
+		data = b""
+		while b"\r\n\r\n" not in data:
+			data += s.recv(4096)
 
-    if b"200" not in data.split(b"\r\n")[0]:
-        raise Exception("CONNECT failed: " + data.decode())
+		if b"200" not in data.split(b"\r\n")[0]:
+			raise Exception("CONNECT failed: " + data.decode())
+
+	except:
+		print("THERE IS SOME ISSUE")
 
 
 
@@ -52,6 +56,3 @@ tunnel(s, "www.google.com", 80)
 
 s.sendall(b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
 print(s.recv(4096).decode())
-
-
-
